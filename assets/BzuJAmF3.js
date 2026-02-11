b5f10f28f3c2638aeff8414973b31b6e6bdb0da9
Rohitkumar Asare <rohitkumar.asare@accenture.com>
2026-02-11 13:33:07 +1300

    Updates

---
+++ assets/BzuJAmF3.js
@@ -0,0 +1,1 @@
+import{S as s}from"./wr0TlTEO.js";import{u}from"./9ozSTxrR.js";function i(e,a){const{title:r,titleTemplate:o,...m}=e;return u({title:r,titleTemplate:o,_flatMeta:m},{...a,transform(t){const n=s({...t._flatMeta});return delete t._flatMeta,{...t,meta:n}}})}export{i as u};
---
+++ assets/DBN5UhSK.js
@@ -0,0 +1,1 @@
+import{K as s}from"./wr0TlTEO.js";const i=s({items:[],add(t){this.items.unshift({key:Symbol(),...t})},remove(t){this.items.splice(t,1)}});export{i as t};
